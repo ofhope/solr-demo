2ada4c6754d88c14a373356cdbfd50cf6e695b72
Alexis <alexis@cvofhope.com>
2013-08-06 07:58:07 +1000

    Init first commit

---
+++ server/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This file is for testing purposes
+# Start with ./start.sh stop with Ctrl+C
+
+SOLR_HOME="../solr"
+LOG_FILE="logging.properties"
+
+java -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -Dsolr.solr.home=$SOLR_HOME -Djava.util.logging.config.file="$SOLR_HOME/$LOG_FILE" -jar start.jar
