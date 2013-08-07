3693db591dc72257e2a9f7f597e29e366dda5ca0
Alexis Hope <bambishope@gmail.com>
2013-08-07 14:06:07 +1000

    update conf

--- server/start.sh
+++ server/start.sh
@@ -6,4 +6,7 @@
 SOLR_HOME="../solr"
 LOG_FILE="logging.properties"
 
-java -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -Dsolr.solr.home=$SOLR_HOME -Djava.util.logging.config.file="$SOLR_HOME/$LOG_FILE" -jar start.jar
+#java -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -Dsolr.solr.home=$SOLR_HOME -Djava.util.logging.config.file="$SOLR_HOME/$LOG_FILE" -jar start.jar
+
+java -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -Dsolr.solr.home=$SOLR_HOME -jar start.jar
+
